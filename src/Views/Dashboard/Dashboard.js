4cd2ea20f9b460f205fe387c307c8fbddf4636ee
Autin Mitra <autin123@gmail.com>
2020-02-29 17:16:01 -0500

    Dashboard + SVG completed

--- src/Views/Dashboard/Dashboard.js
+++ src/Views/Dashboard/Dashboard.js
@@ -5,6 +5,13 @@ import './Dashboard.scss';
 import 'styles/colors.scss';
 import DashboardCard from './DashboardCard/DashboardCard';
 
+import heartSVG from './images/dashboard_heart.svg';
+import lungSVG from './images/dashboard_lung.svg';
+import neurologicalSVG from './images/dashboard_brain.svg';
+import cancerSVG from './images/dashboard_cancer.svg';
+import cautionsSVG from './images/dashboard_cautions.svg';
+
+
 class Dashboard extends React.Component {
   render() {
     let rowClass = classNames("flex flex-wrap flex-col md:flex-row w-full dashboard max-w-screen-lg");
@@ -12,13 +19,23 @@ class Dashboard extends React.Component {
     return (
       <div className="flex flex-wrap w-full lg:-mx-4 -my-4">
         <div className={rowClass}>
-          <DashboardCard className="yel" flex="3"/>
-          <DashboardCard className="bg-accent-green" flex="4" />
+          <DashboardCard img={heartSVG} title="Heart" className="red text-white" flex="3">
+            <span className="z-10">Risks: Heart Attack</span>
+          </DashboardCard>
+          <DashboardCard img={lungSVG} title="Lung" className="purple text-white" flex="4">
+            <span className="z-10">Risks: Asthma</span>
+          </DashboardCard>
         </div>
         <div className={rowClass}>
-          <DashboardCard className="yel" flex="1"/>
-          <DashboardCard className="bg-accent-green" flex="1" />
-          <DashboardCard className="bg-accent-green" flex="1" />
+          <DashboardCard img={neurologicalSVG} title="Neurological" className="green" flex="1">
+            <span className="z-10">Risks: Parkinsons, Alzheimers</span>
+          </DashboardCard>
+          <DashboardCard img={cancerSVG} title="Cancer" className="darkblue text-white" flex="1">
+            <span className="z-10">Deteced types: None</span>
+          </DashboardCard>
+          <DashboardCard img={cautionsSVG} title="Cautions" className="yellow" flex="1">
+            <span className="z-10">Other Risks: Influenza</span>
+          </DashboardCard>
         </div>
       </div>
     );
--- src/Views/Dashboard/DashboardCard/DashboardCard.js
+++ src/Views/Dashboard/DashboardCard/DashboardCard.js
@@ -9,9 +9,13 @@ class DashboardCard extends React.Component {
       <Link 
         to="/" 
         style={{flex: this.props.flex ?? 1}} 
-        className={"dash-card rounded-lg relative shadow-lg hover:shadow-xl outline-none " + this.props.className}
+        className={"dash-card rounded-lg relative shadow-lg hover:shadow-xl outline-none overflow-hidden " + this.props.className}
       >
-
+        <img className="absolute object-cover w-full h-full top-0" src={this.props.img}></img>
+        <div class="w-full h-full p-6 flex flex-col justify-end">
+          <span className="card-title">{this.props.title}</span>
+          {this.props.children}
+        </div>
       </Link>
     )
   }
